4a89166238f7dbb7154fcf2b26ac8120e4569416
Denis Dyakov <denis.dyakov@gmail.com>
2024-06-08 13:19:35 +0500

    Fix an issue with json parsing and other improvements

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-JSON and JSONC bash parser
-==========================
+JSON/JSONC bash parser
+======================
 
 About
 -----
+
 Library `json.sh` allows you to parse JSON and JSONC (JSON superset with comments) formats in BASH and manipulate the parsed data to quickly select and search results.
 This library does not claim to be high performance, but has been tested with JSON/JSONC configuration files on x86 and ARM Linux devices.
 
 Features
 --------
+
 * compatible with standard JSON and unofficial JSONC formats
 * no dependencies on any libraries (only standard BASH environment)
 * incremental single-pass parsing
@@ -18,8 +20,10 @@ Features
 
 Compliance with the standard
 ----------------------------
+
 There are a lot of discussions that JSON standard is not clearly defined in some parts over the years, and parsers may differ from each other in some tricky cases.
 So I just put here briefly what supported and how:
+
 * Objects and Arrays
 * Strings (as keys and values):
   * Verify escaped characters
@@ -33,6 +37,7 @@ So I just put here briefly what supported and how:
 
 Usage
 -----
+
 The parser itself is located in the file `json.sh`.
 
 You can test parser from command line with the script `examples/json_parse_file.sh` to parse any JSON/JSONC file to get results (debug tokens output).
@@ -40,6 +45,7 @@ You can test parser from command line with the script `examples/json_parse_file.
 To embed the parser to your BASH decision, please, read explanation below.
 
 Let's parse real JSON data:
+
 ``` json
 {"menu": {
   "id": "file",
@@ -53,12 +59,16 @@ Let's parse real JSON data:
   },
 }}
 ```
+
 First, load library in your BASH script:
+
 ``` bash
 # Load json library
 source ./json.sh
 ```
+
 Then, try to parse JSON taken from file:
+
 ``` bash
 parse_json "<path to json file>"; retval=$?
 if [[ $retval -ne 0 ]]; then
@@ -66,75 +76,94 @@ if [[ $retval -ne 0 ]]; then
 	exit 1
 fi
 ```
+
 , either if we want to read JSON from pipe:
+
 ``` bash
 parse_json_from_pipe < <(func_return_heredoc_with_json); retval=$?
 if [[ $retval -ne 0 ]]; then
-	echo "Error parsing JSON in line $JSON_LINE_ERROR" 1>&2
-	exit 1
+  echo "Error parsing JSON in line $JSON_LINE_ERROR" 1>&2
+  exit 1
 fi
 ```
+
 In case of success, select parsing results. Get value with function `get_json_path_value` call, pointing search path via parameters:
+
 ``` bash
-val=$(get_json_path_value "menu" "id")
+local val; get_json_path_value "val" "menu" "id"
 echo "- Select menu/id value = $val"
-val=$(get_json_path_value "menu" "type")
+local val; get_json_path_value "val" "menu" "type"
 echo "- Select menu/type value = $val"
 ```
+
 , with output:
+
 ```
 - Select menu/id value = file
 - Select menu/type value = Word document
 ```
+
 Please note, that with `get_json_path_value` you can also index JSON arrays with integer values:
+
 ``` bash
-val=$(get_json_path_value "menu" "popup" "menuitem" 1 "args" 2)
+local val; get_json_path_value "val" "menu" "popup" "menuitem" 1 "args" 2
 echo "- Select menu/popup/menuitem/1/args/2 value = $val"
-val=$(get_json_path_value "menu" "popup" "menuitem" 0 "args" 3)
+local val; get_json_path_value "val" "menu" "popup" "menuitem" 0 "args" 3
 echo "- Select menu/popup/menuitem/0/args/3 value with unicode = $val"
 ```
+
 , with output:
+
 ```
 - Select menu/popup/menuitem/1/args/2 value = 3.1415926
 - Select menu/popup/menuitem/0/args/3 value with unicode = Яndex
 ```
+
 You can find additional usage demo in folder `examples` with BASH scripts: `parse_json_example1.sh`, `parse_json_example2.sh`, `json_parse_file.sh`.
 
 API
 ---
+
 API is limited to following recommended functions:
+
 * `parse_json_from_pipe`: read and parse JSON taken from pipe.
   Return 0 in case of success, either non zero error code. In case of error `JSON_LINE_ERROR` variable pointing to the line where error found.
   > *Note*: You don't need to care about, but as a success parse result next internal variables filled with parsed information (tokens):
-  >  - `jt_type` - type (object, array, string, primitive)
-  >  - `jt_content` - content for strings and primitives
-  >  - `jt_parent` - reference to parent token
-  >  - `jt_size` - number of children constructions
+  > * `jt_type` - type (object, array, string, primitive)
+  > * `jt_content` - content for strings and primitives
+  > * `jt_parent` - reference to parent token
+  > * `jt_size` - count of direct children subtrees in json structure
+  > * `jt_size_tok` - total number of all child tokens
+
 * `parse_json`: do the same as `parse_json_from_pipe`, but take data from file path passed via parameter.
-* `get_json_path_value`: select JSON value with search path provided via parameters passed to the function. Can return JSON array size, if search path point to the array [...] in JSON document. The search path elements are compared with JSON key elements to find matches and it working in case insensitive mode.
-* `print_json_tokens`: for debug purpose. Print the content of variables `jt_type`, `jt_content`, `jt_parent`, `jt_size` as a result of successful execution of `parse_json` either `parse_json_from_pipe` call.
+* `get_json_path_value`: select JSON value with search path provided via parameters passed to the function from 2nd argument. Function return result to the variable which name specified in 1st argument. Can return JSON array size, if search path point to the array [...] in JSON document. The search path elements are compared with JSON key elements to find matches and it working in case insensitive mode.
+* `print_json_tokens`: for debug purpose. Print the content of variables `jt_type`, `jt_content`, `jt_parent`, `jt_size`, `jt_size_tok` as a result of successful execution of `parse_json` either `parse_json_from_pipe` call.
 
 Unit-tests
 ----------
+
 You can run `json_unit_tests.sh` to verify that library is in working condition in your environment.
 The output must contain only "`Test passed`" results, like this:
+
 ```
-Test passed. No error parsing JSON
-Test passed. No error parsing JSON
+Test #20 PASSED. Success status=0. No error parsing JSON/JSONC
+Test #21 PASSED. Success status=0. Result=3.1415926. Expected value for menu/popup/menuitem/0/args/2=3.1415926
 ...
-Test passed. Error parsing JSON in line 8
-Test passed. Error parsing JSON in line 12
+Test #56 PASSED. Success status=2. Error parsing JSON/JSONC in line 9
+Test #57 PASSED. Success status=1. Error parsing JSON/JSONC in line 9
 ```
+
 In case of errors, please, report to [issue tracker](https://github.com/d2r2/jsonc-bash/issues).
 
 Credits
 -------
+
 This library is inspired by minimalistic JSON parser written by Serge Zaitsev: https://github.com/zserge/jsmn.
 
 Links
 -----
-* Test suite which cover a lot of parsers, but only one BASH parser found there. Keeps big collection of JSON files to check parser compliance with the standard: https://github.com/nst/JSONTestSuite
 
+* Test suite used which cover a lot of parsers, but only one BASH parser found there. Keeps big collection of JSON files to check parser compliance with the standard: https://github.com/nst/JSONTestSuite
 
 License
 -------
--- json_unit_tests.sh
+++ json_unit_tests.sh
@@ -3,16 +3,14 @@
 #
 # MIT License
 #
-# Copyright (c) 2021 Denis Dyakov <denis.dyakov@gma**.com>
+# Copyright (c) 2021-2024 Denis Dyakov <denis.dyakov@gma**.com>
 #
 # Inspired by minimalistic JSON parser written
 # by Serge Zaitsev: https://github.com/zserge/jsmn.
 # This code can be considered rewritten from
 # the original project with some modifications.
 #
-# Support standard JSON, as well as its superset -
-# JSONC (JSON with comments). Parsing comply with
-# JSON standard and allow comments (JSONC extension).
+# Support parsing of regular JSON, as well as JSONC (JSON with comments).
 #
 
 # Load JSON library
@@ -40,20 +38,39 @@ get_json_struct_with_no_error_1() {
 # no error, to test error handling.
 get_json_struct_with_no_error_2() {
 	cat <<-EOF
-{"menu": {
-  // Put one line comment here
-  "id": "file",
-  "type": "Word document",
-  "popup": {
-    "menuitem": [
-      {"value": "Open", "onclick": "OpenDoc", "args": [1, "some text", 3.1415926, "\u0418"] },
-      /*
-        And multiline comment here
-      */
-      {"value": "Close", "onclick": "CloseDoc"}
-    ],
-  },
-}}
+{
+  "menu": {
+    // Put one line comment here
+    "id": "file",
+    "type": "Word document",
+    "popup": {
+      "menuitem": [
+        {"value": "Open", "onclick": "OpenDoc", "args": [1, "some text", 3.1415926, "\u0418"] },
+        /*
+          And multiline comment here
+        */
+        {"value": "Close", "onclick": "CloseDoc"}
+      ],
+    },
+  }
+}
+	EOF
+}
+
+
+# Function return regular JSONC sample with
+# no error, to test error handling.
+get_json_struct_with_no_error_3() {
+	cat <<-EOF
+{
+	// Set CPU governor and frequencies range.
+	// Use utility cpufreq-info and cpufreq-set. 
+	"cpu": {
+		"governor": "powersave",
+		//"governor": "schedutil",
+		"frequency": { "min": 408000, "max": 816000 }
+	}
+}
 	EOF
 }
 
@@ -167,110 +184,282 @@ get_json_struct_with_error_6() {
 	EOF
 }
 
-# Unit test. Parse JSON with no error
-parse_json_from_pipe < <(get_json_struct_with_no_error_1); retval=$?
-if [[ $retval -eq 0 ]]; then
-	echo "Test passed. No error parsing JSON" 1>&2
-else
-	echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
-	exit 1
-fi
+# Function return regular JSON sample with error
+# on purpuse, to test error handling.
+# Comma is missing
+get_json_struct_with_error_7() {
+	cat <<-EOF
+{"menu": {
+  "id": "file",
+  "type": "Word document",
+  "popup": {
+    "menuitem": [
+      {"value": "Close", "onclick": "CloseDoc"}
+    ],
+  }
+  "system": {
+    "custom_scripts": [
+      "fail2ban-mqtt"
+    ]
+  }
+  }}
+	EOF
+}
 
-# Unit test. Parse JSON with no error
-parse_json_from_pipe < <(get_json_struct_with_no_error_2); retval=$?
-if [[ $retval -eq 0 ]]; then
-	echo "Test passed. No error parsing JSON" 1>&2
-else
-	echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
-	exit 1
-fi
+# Function return regular JSON sample with error
+# on purpuse, to test error handling.
+# Comma is missing
+get_json_struct_with_error_8() {
+	cat <<-EOF
+{"menu": {
+  "id": "file",
+  "type": "Word document",
+  "popup": {
+    "menuitem": [
+      {"value": "Close", "onclick": "CloseDoc"}
+    ]
+  }
+  {"value2": "Close", "onclick2": "CloseDoc"}
+  }}
+	EOF
+}
 
-# Unit test. Parse JSON with no error and search path value
-parse_json_from_pipe < <(get_json_struct_with_no_error_2); retval=$?
-if [[ $retval -eq 0 ]]; then
-  # Search value
-  val=$(get_json_path_value "menu" "popup" "menuitem" "0" "args" 2); retval=$?
-  if [[ $retval -eq 0 && "$val" == "3.1415926" ]]; then
-	  echo "Test passed. Value for menu/popup/menuitem/0/args/2 = $val" 1>&2
+join_str() {
+  local separator="$1"
+  shift
+  local first="$1"
+  shift
+  printf "%s" "$first" "${@/#/$separator}"
+}
+
+print_val() {
+  local val="$1"
+  [[ "$val" == "" ]] && echo -n "<empty>" || echo -n "$val"
+}
+
+run_tests() {
+  # Test 10
+  # Unit test. Parse JSON/JSONC with no error
+  local test_no="10"
+  parse_json_from_pipe < <(get_json_struct_with_no_error_1); local status=$?
+  if [[ $status -eq 0 ]]; then
+    echo "Test #${test_no} PASSED. Success status=$status. No error parsing JSON/JSONC" 1>&2
   else
-	  echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
+    echo "Test #${test_no} FAILED!!! Success status=$status. Report to developer or fix youself (and report to developer)!!!"
+    exit 1
   fi
-  # Search value for not matching uppper/lower case path (nevertheless it should work)
-  val=$(get_json_path_value "Menu" "popuP" "menUitem" "0" "ARgs" 3); retval=$?
-  if [[ $retval -eq 0 && "$val" == "И" ]]; then
-	  echo "Test passed. Value for Menu/popuP/menUitem/0/ARgs/3 = $val" 1>&2
+  #local xpath=("menu" "id" "menuitem" "0" "args" 2)
+  #local val; get_json_path_value "val" ${xpath[@]}; local status=$?  
+  #print_json_tokens
+
+  #exit
+
+  # Test 11
+  # Unit test. Parse JSON/JSONC with no error
+  test_no="11"
+  parse_json_from_pipe < <(get_json_struct_with_no_error_2); local status=$?
+  if [[ $status -eq 0 ]]; then
+    echo "Test #${test_no} PASSED. Success status=$status. No error parsing JSON/JSONC" 1>&2
   else
-	  echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
+    echo "Test #${test_no} FAILED!!! Success status=$status. Report to developer or fix youself (and report to developer)!!!"
+    exit 1
   fi
-  # Search value for invalid path (exceeding array size)
-  val=$(get_json_path_value "menu" "popup" "menuitem" "0" "args" 4); retval=$?
-  if [[ $retval -ne 0 ]]; then
-	  echo "Test passed. Value for menu/popup/menuitem/0/args/4 is not found" 1>&2
+
+  # Test 20
+  # Unit test. Parse JSON/JSONC with no error and get value from XPath search
+  test_no="20"
+  parse_json_from_pipe < <(get_json_struct_with_no_error_2); local status=$?
+  if [[ $status -eq 0 ]]; then
+    echo "Test #${test_no} PASSED. Success status=$status. No error parsing JSON/JSONC" 1>&2
+    # Test 21
+    # Search value for valid path
+    test_no="21"
+    local xpath=("menu" "popup" "menuitem" "0" "args" 2)
+    local val; get_json_path_value "val" ${xpath[@]}; local status=$?
+    local expected_val="3.1415926"
+    if [[ $status -eq 0 && "$val" == "$expected_val" ]]; then
+      echo "Test #${test_no} PASSED. Success status=$status. Result=$(print_val $val). Expected value for $(join_str "/" ${xpath[@]})=$expected_val" 1>&2
+    else
+      echo "Test #${test_no} FAILED!!! Success status=$status. Result=$(print_val $val). Report to developer or fix youself (and report to developer)!!!"
+    fi
+    # Test 22
+    # Search value for not matching upper/lower case, bu still valid path (nevertheless it should work)
+    test_no="22"
+    local xpath=("Menu" "popuP" "menUitem" "0" "ARgs" 3)
+    local val; get_json_path_value "val" ${xpath[@]}; local status=$?
+    local expected_val="И"
+    if [[ $status -eq 0 && "$val" == "И" ]]; then
+      echo "Test #${test_no} PASSED. Success status=$status. Result=$(print_val $val). Expected value for $(join_str "/" ${xpath[@]})=$expected_val" 1>&2
+    else
+      echo "Test #${test_no} FAILED!!! Success status=$status. Result=$(print_val $val). Report to developer or fix youself (and report to developer)!!!"
+    fi
+    # Test 23
+    # Search value for invalid path (exceeding array size)
+    test_no="23"
+    local xpath=("menu" "popup" "menuitem" "0" "args" 4)
+    local val; get_json_path_value "val" ${xpath[@]}; local status=$?
+    if [[ $status -ne 0 ]]; then
+      echo "Test #${test_no} PASSED. Success status=$status. Result=$(print_val $val). Value for $(join_str "/" ${xpath[@]}) is not found" 1>&2
+    else
+      echo "Test #${test_no} FAILED!!! Success status=$status. Result=$(print_val $val). Report to developer or fix youself (and report to developer)!!!"
+    fi
+    # Test 24
+    # Search value for invalid path (menu-item and menuitem doesn't match)
+    test_no="24"
+    local xpath=("menu" "popup" "menu-item" "0" "args" 1)
+    local val; get_json_path_value "val" ${xpath[@]}; local status=$?
+    if [[ $status -ne 0 ]]; then
+      echo "Test #${test_no} PASSED. Success status=$status. Result=$(print_val $val). Value for $(join_str "/" ${xpath[@]}) is not found" 1>&2
+    else
+      echo "Test #${test_no} FAILED!!! Success status=$status. Result=$(print_val $val) Report to developer or fix youself (and report to developer)!!!"
+    fi
+    # Test 25
+    # Search for invalid path, and verify that $val is reassigned to empty value
+    test_no="25"
+    local xpath=("menu1" "popup" "menu-item" "0" "args" 1)
+    local val="asdasd"; get_json_path_value "val" ${xpath[@]}; local status=$?
+    local expected_val=""
+    if [[ $status -ne 0 && "$val" == "$expected_val" ]]; then
+      echo "Test #${test_no} PASSED. Success status=$status. Result=$(print_val $val). Expected value for $(join_str "/" ${xpath[@]})=$(print_val $expected_val)" 1>&2
+    else
+      echo "Test #${test_no} FAILED!!! Success status=$status. Result=$(print_val $val). Report to developer or fix youself (and report to developer)!!!"
+    fi
   else
-	  echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
+    echo "Test #${test_no} FAILED!!! Success status=$status. Result=$(print_val $val). Report to developer or fix youself (and report to developer)!!!"
+    exit 1
   fi
-  # Search value for invalid path (menu-item and menuitem doesn't match)
-  val=$(get_json_path_value "menu" "popup" "menu-item" "0" "args" 1); retval=$?
-  if [[ $retval -ne 0 ]]; then
-	  echo "Test passed. Value for menu/popup/menu-item/0/args/1 is not found" 1>&2
+
+  # Test 30
+  # Unit test. Parse JSON/JSONC with no error and get value from XPath search
+  test_no="30"
+  parse_json_from_pipe < <(get_json_struct_with_no_error_3); local status=$?
+  if [[ $status -eq 0 ]]; then
+    echo "Test #${test_no} PASSED. Success status=$status. No error parsing JSON/JSONC" 1>&2
+    # Test 31
+    # Search value for valid path
+    test_no="31"
+    local xpath=("cpu" "governor")
+    local val; get_json_path_value "val" ${xpath[@]}; local status=$?
+    local expected_val="powersave"
+    if [[ $status -eq 0 && "$val" == "$expected_val" ]]; then
+      echo "Test #${test_no} PASSED. Success status=$status. Result=$(print_val $val). Expected value for $(join_str "/" ${xpath[@]})=$expected_val" 1>&2
+    else
+      echo "Test #${test_no} FAILED!!! Success status=$status. Result=$(print_val $val). Report to developer or fix youself (and report to developer)!!!"
+    fi
+    # Test 32
+    # Search value for valid path
+    test_no="32"
+    local xpath=("cpu" "frequency" "min")
+    local val; get_json_path_value "val" ${xpath[@]}; local status=$?
+    local expected_val="408000"
+    if [[ $status -eq 0 && "$val" == "$expected_val" ]]; then
+      echo "Test #${test_no} PASSED. Success status=$status. Result=$(print_val $val). Expected value for $(join_str "/" ${xpath[@]})=$expected_val" 1>&2
+    else
+      echo "Test #${test_no} FAILED!!! Success status=$status. Result=$(print_val $val). Report to developer or fix youself (and report to developer)!!!"
+    fi
+    # Test 33
+    # Search value for valid path
+    test_no="33"
+    local xpath=("cpu" "frequency" "max")
+    local val; get_json_path_value "val" ${xpath[@]}; local status=$?
+    local expected_val="816000"
+    if [[ $status -eq 0 && "$val" == "$expected_val" ]]; then
+      echo "Test #${test_no} PASSED. Success status=$status. Result=$(print_val $val). Expected value for $(join_str "/" ${xpath[@]})=$expected_val" 1>&2
+    else
+      echo "Test #${test_no} FAILED!!! Success status=$status. Result=$(print_val $val). Report to developer or fix youself (and report to developer)!!!"
+    fi
+fi
+
+  # Test 50
+  # Unit test. Parse JSON/JSONC with error on purpose
+  test_no="50"
+  parse_json_from_pipe < <(get_json_struct_with_error_1); local status=$?
+  if [[ $status -ne 0 ]]; then
+    echo "Test #${test_no} PASSED. Success status=$status. Error parsing JSON/JSONC in line $JSON_LINE_ERROR" 1>&2
   else
-	  echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
+    echo "Test #${test_no} FAILED!!! Success status=$status. Report to developer or fix youself (and report to developer)!!!"
+    exit 1
   fi
-else
-	echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
-	exit 1
-fi
 
-# Unit test. Parse JSON with error on purpose
-parse_json_from_pipe < <(get_json_struct_with_error_1); retval=$?
-if [[ $retval -ne 0 ]]; then
-	echo "Test passed. Error parsing JSON in line $JSON_LINE_ERROR" 1>&2
-else
-	echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
-	exit 1
-fi
+  # Test 51
+  # Unit test. Parse JSON/JSONC with error on purpose
+  test_no="51"
+  parse_json_from_pipe < <(get_json_struct_with_error_2); local status=$?
+  if [[ $status -ne 0 ]]; then
+    echo "Test #${test_no} PASSED. Success status=$status. Error parsing JSON/JSONC in line $JSON_LINE_ERROR" 1>&2
+  else
+    echo "Test #${test_no} FAILED!!! Success status=$status. Report to developer or fix youself (and report to developer)!!!"
+    exit 1
+  fi
 
-# Unit test. Parse JSON with error on purpose
-parse_json_from_pipe < <(get_json_struct_with_error_2); retval=$?
-if [[ $retval -ne 0 ]]; then
-	echo "Test passed. Error parsing JSON in line $JSON_LINE_ERROR" 1>&2
-else
-	echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
-	exit 1
-fi
+  # Test 52
+  # Unit test. Parse JSON/JSONC with error on purpose
+  test_no="52"
+  parse_json_from_pipe < <(get_json_struct_with_error_3); local status=$?
+  if [[ $status -ne 0 ]]; then
+    echo "Test #${test_no} PASSED. Success status=$status. Error parsing JSON/JSONC in line $JSON_LINE_ERROR" 1>&2
+  else
+    echo "Test #${test_no} FAILED!!! Success status=$status. Report to developer or fix youself (and report to developer)!!!"
+    exit 1
+  fi
 
-# Unit test. Parse JSON with error on purpose
-parse_json_from_pipe < <(get_json_struct_with_error_3); retval=$?
-if [[ $retval -ne 0 ]]; then
-	echo "Test passed. Error parsing JSON in line $JSON_LINE_ERROR" 1>&2
-else
-	echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
-	exit 1
-fi
+  # Test 53
+  # Unit test. Parse JSON/JSONC with error on purpose
+  test_no="53"
+  parse_json_from_pipe < <(get_json_struct_with_error_4); local status=$?
+  if [[ $status -ne 0 ]]; then
+    echo "Test #${test_no} PASSED. Success status=$status. Error parsing JSON/JSONC in line $JSON_LINE_ERROR" 1>&2
+  else
+    echo "Test #${test_no} FAILED!!! Success status=$status. Report to developer or fix youself (and report to developer)!!!"
+    exit 1
+  fi
 
-# Unit test. Parse JSON with error on purpose
-parse_json_from_pipe < <(get_json_struct_with_error_4); retval=$?
-if [[ $retval -ne 0 ]]; then
-	echo "Test passed. Error parsing JSON in line $JSON_LINE_ERROR" 1>&2
-else
-	echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
-	exit 1
-fi
+  # Test 54
+  # Unit test. Parse JSON/JSONC with error on purpose
+  test_no="54"
+  parse_json_from_pipe < <(get_json_struct_with_error_5); local status=$?
+  if [[ $status -ne 0 ]]; then
+    echo "Test #${test_no} PASSED. Success status=$status. Error parsing JSON/JSONC in line $JSON_LINE_ERROR" 1>&2
+  else
+    echo "Test #${test_no} FAILED!!! Success status=$status. Report to developer or fix youself (and report to developer)!!!"
+    exit 1
+  fi
 
-# Unit test. Parse JSON with error on purpose
-parse_json_from_pipe < <(get_json_struct_with_error_5); retval=$?
-if [[ $retval -ne 0 ]]; then
-	echo "Test passed. Error parsing JSON in line $JSON_LINE_ERROR" 1>&2
-else
-	echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
-	exit 1
-fi
+  # Test 55
+  # Unit test. Parse JSON/JSONC with error on purpose
+  test_no="55"
+  parse_json_from_pipe < <(get_json_struct_with_error_6); local status=$?
+  if [[ $status -ne 0 ]]; then
+    echo "Test #${test_no} PASSED. Success status=$status. Error parsing JSON/JSONC in line $JSON_LINE_ERROR" 1>&2
+  else
+    echo "Test #${test_no} FAILED!!! Success status=$status. Report to developer or fix youself (and report to developer)!!!"
+    exit 1
+  fi
 
-# Unit test. Parse JSON with error on purpose
-parse_json_from_pipe < <(get_json_struct_with_error_6); retval=$?
-if [[ $retval -ne 0 ]]; then
-	echo "Test passed. Error parsing JSON in line $JSON_LINE_ERROR" 1>&2
-else
-	echo "Test failed!!! Report to developer or fix youself (and report to developer)!!!"
-	exit 1
-fi
+  # Test 56
+  # Unit test. Parse JSON/JSONC with error on purpose
+  test_no="56"
+  parse_json_from_pipe < <(get_json_struct_with_error_7); local status=$?
+  #print_json_tokens
+  if [[ $status -ne 0 ]]; then
+    echo "Test #${test_no} PASSED. Success status=$status. Error parsing JSON/JSONC in line $JSON_LINE_ERROR" 1>&2
+  else
+    echo "Test #${test_no} FAILED!!! Success status=$status. Report to developer or fix youself (and report to developer)!!!"
+    exit 1
+  fi
+
+  # Test 57
+  # Unit test. Parse JSON/JSONC with error on purpose
+  test_no="57"
+  parse_json_from_pipe < <(get_json_struct_with_error_8); local status=$?
+  #print_json_tokens
+  if [[ $status -ne 0 ]]; then
+    echo "Test #${test_no} PASSED. Success status=$status. Error parsing JSON/JSONC in line $JSON_LINE_ERROR" 1>&2
+  else
+    echo "Test #${test_no} FAILED!!! Success status=$status. Report to developer or fix youself (and report to developer)!!!"
+    exit 1
+  fi
+}
+
+JSON_WARN_INVALID_PATH_INDEX=0 # disable WARNING report on invalid XPath search array index
+run_tests
